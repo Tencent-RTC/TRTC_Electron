c2bcf1a36376ba0d5ba3f72897e9518a6fcd8a3a
zackshi <zackshi@tencent.com>
2023-02-10 15:02:12 +0800

    release: TRTC Electron Simple demo v1.0.4

--- TRTCSimpleDemo/preload.electron.js
+++ TRTCSimpleDemo/preload.electron.js
@@ -4,3 +4,15 @@ console.log(`[preload.js] electron version: ${process.versions.electron}`);
 console.log(`[preload.js] process.cwd(): ${process.cwd()}`);
 console.log(`[preload.js] __dirname: ${__dirname}`);
 console.log(`[preload.js] env.NODE_ENV: ${process.env.NODE_ENV}`);
+
+const { ipcRenderer } = require('electron');
+// 打印来自主进程的奔溃 dump 文件存放目录
+ipcRenderer.on('crash-file-path', (event, args) => {
+  console.warn('crash-file-path:', args);
+});
+
+// // crash test
+// setTimeout(() => {
+//   console.warn('---------------test crash');
+//   process.crash();
+// }, 10 * 1000);
