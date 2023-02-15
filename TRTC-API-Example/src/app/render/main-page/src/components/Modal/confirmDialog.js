c213ad0c9320d3db7f4ea0b9de18d19989470fcc
zackshi <zackshi@tencent.com>
2023-02-15 12:00:11 +0800

    release: TRTC Electron API Examples v1.0.3

--- TRTC-API-Example/src/app/render/main-page/src/components/Modal/confirmDialog.js
+++ TRTC-API-Example/src/app/render/main-page/src/components/Modal/confirmDialog.js
@@ -23,6 +23,7 @@ export default function ConfirmDialog() {
     Modal.confirm = (confirmProps) => {
       addConfirm(confirmProps);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const addConfirm = (confirmProps) => {
